5dd2691bd21fd57805a4d8b6cbccf2286fb61f4b
yoshi2-2 <netopiku@gmail.com>
2018-12-23 20:35:29 +0900

    deviseストロングパラメーター追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,5 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery #追加
-//= require jquery_ujs #追加
+//= require jquery
+//= require jquery_ujs
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
 
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :phone_number, :avatar, :introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :phone_number, :avatar, :introduction])
+  end
 
   private
 
