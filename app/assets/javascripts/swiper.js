d2ca77630a770c146f2ef655f74d4d60a53787a2
yoshi2-2 <netopiku@gmail.com>
2018-12-24 23:01:07 +0900

    トップページヘッダーCSS

---
+++ app/assets/javascripts/swiper.js
@@ -0,0 +1,13 @@
+window.onload = load;
+function load(){
+var swiper = new Swiper('.swiper-container', {
+pagination: '.swiper-pagination',
+nextButton: '.swiper-button-next',
+prevButton: '.swiper-button-prev',
+paginationClickable: true,
+spaceBetween: 30,
+centeredSlides: true,
+autoplay: 2500,
+autoplayDisableOnInteraction: false
+});
+}
